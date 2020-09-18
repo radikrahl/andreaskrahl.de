89138ab7c5bb47df8b2b238644b46e8ea5a80472
andreas.krahl <andi@radikrahl.com>
2020-09-18 22:13:44 +0200

    test headless

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,8 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
+    // browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
   });
