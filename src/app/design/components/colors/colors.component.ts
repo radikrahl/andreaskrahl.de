253a15bcff6e8dc60631bb8e0c4193f141f046ce
Andreas Krahl <a.krahl90@gmail.com>
2021-11-02 21:28:31 +0100

    added components

---
+++ src/app/design/components/colors/colors.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-design-colors',
+  templateUrl: './colors.component.html',
+  styleUrls: ['./colors.component.scss'],
+})
+export class ColorsComponent {}
--- src/app/design/design.module.ts
+++ src/app/design/design.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { LazyLoadedChildRouteService } from '../core/services/lazyroute.service';
 import { SharedModule } from '../shared/shared.module';
+import { ButtonsComponent } from './components/buttons/buttons.component';
+import { ColorsComponent } from './components/colors/colors.component';
 import { DesignComponent } from './components/design.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { TypographyComponent } from './components/typography/typography.component';
 import { DesignRoutingModule } from './design-routing.module';
 
 @NgModule({
   imports: [DesignRoutingModule, SharedModule],
-  declarations: [DesignComponent],
+  declarations: [
+    DesignComponent,
+    ColorsComponent,
+    TypographyComponent,
+    ListsComponent,
+    ButtonsComponent,
+    FormsComponent
+  ],
   providers: [LazyLoadedChildRouteService],
 })
 export class DesignModule {}
