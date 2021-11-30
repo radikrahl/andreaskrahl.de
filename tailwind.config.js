8c112f043e7aafa98286c93deec08f592fe1d84c
Andreas Krahl <a.krahl90@gmail.com>
2021-11-30 19:15:15 +0100

    switched to @use syntax; introduced tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  /*
+   * disabled right now, because it needs a main.css available
+   * need to fix it in future
+   */
+  // mode: "jit", // Just-In-Time Compiler
+  // purge: ["./**/*.hbs"],
+  theme: {
+      extend: {},
+      screens: {
+          'xsm': '32rem',
+          'sm': '48rem',
+          'md': '64rem',
+          'lg': '80rem',
+          'xl': '90rem'
+      }
+  },
+  corePlugins: [
+      /*
+       * grid plugins
+       */
+      'gridColumn',
+      'gridTemplateColumns',
+      'gap',
+
+      /*
+       * spacing plugins
+       */
+      'padding',
+      'margin',
+
+      /*
+       * typography
+      */
+      'textTransform',
+      'fontWeight'
+  ]
+};
