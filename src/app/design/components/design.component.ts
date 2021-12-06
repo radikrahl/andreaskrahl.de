1e795b00ed28bad48781d6a9e7ea3c34828d9ef8
Andreas Krahl <a.krahl90@gmail.com>
2021-12-06 21:09:28 +0100

    introduced side navigation style; WIP burger menu refactor

--- src/app/design/components/design.component.ts
+++ src/app/design/components/design.component.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
-import { ActivatedRoute, Router, Routes } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Routes } from '@angular/router';
 import { LazyLoadedChildRouteService } from 'src/app/core/services/lazyroute.service';
 
 @Component({
   selector: 'app-design',
   templateUrl: './design.component.html',
-  styles: ['.router-link-active{ color: var(--color-links-hover)}'],
+  styleUrls: ['./design.component.scss'],
 })
 export class DesignComponent {
   public childRoutes: Routes;
+  @ViewChild('sideNav') sideNav: ElementRef;
+
   constructor(private lazyRoutes: LazyLoadedChildRouteService) {
     this.childRoutes = this.lazyRoutes.getChildRoutes(DesignComponent);
+  }
+
+  toggleSideNav(event: MouseEvent) {
+    let element = this.sideNav.nativeElement as HTMLElement;
+    element.classList.toggle('side-nav--open');
   }
 }
