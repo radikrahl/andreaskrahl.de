cf0623935ef96b14af888c4c4824a023115e5a95
Andreas Krahl <a.krahl90@gmail.com>
2022-02-08 18:52:51 +0100

    uncapitalize filename

--- src/app/core/helper/GuidGenerator.ts
+++
@@ -1,9 +0,0 @@
-export class GuidGenerator {
-  public static createGuid(): string {
-    return '_' + Math.random().toString(36);
-  }
-
-  public static createUUID(): string {
-    return this.createGuid().substring(2, 9);
-  }
-}
